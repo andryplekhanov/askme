c486a02afec9e2b7789a0548784c76442eca4ea1
andryplekhanov <1989plekhanov@gmail.com>
2017-05-25 18:47:51 +0300

    create migration AddIndexUsernameToUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522182705) do
+ActiveRecord::Schema.define(version: 20170525153948) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170522182705) do
     t.string   "password_hash"
     t.string   "password_salt"
     t.string   "avatar_url"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
