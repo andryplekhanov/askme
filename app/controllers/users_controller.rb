e2b93cd5da5224dc7e623744e130aa13d4f9636a
andryplekhanov <1989plekhanov@gmail.com>
2017-05-26 07:55:21 +0300

    create UsersController and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.new(
+      name: 'Andrey',
+      username: 'andrey_plekhanov',
+      avatar_url: 'https://pp.userapi.com/c836226/v836226315/12713/1Ko1J3P2j6E.jpg'
+    )
+  end
 end
