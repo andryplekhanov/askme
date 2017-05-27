7da0848d95279dd0cae8c7401a6e8b9ce8fa4088
andryplekhanov <1989plekhanov@gmail.com>
2017-05-27 12:30:51 +0300

    add styles, partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
     @user = User.new(
       name: 'Andrey',
       username: 'andrey_plekhanov',
-      avatar_url: 'https://pp.userapi.com/c836226/v836226315/12713/1Ko1J3P2j6E.jpg'
+      # avatar_url: 'https://pp.userapi.com/c836226/v836226315/12713/1Ko1J3P2j6E.jpg'
     )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('20.03.2000')),
+      Question.new(text: 'Что нового?', created_at: Date.parse('20.03.2000'))
+    ]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'user#index'
+
   resources :users
+
+  get 'show' => 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
